fc5e1d14c378c78717650cc8fce0fb7ad50a725b
shakib-mia <smdshakibmia2001@gmail.com>
2022-12-31 21:19:33 +0600

    structure added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './features/counter/counterSlice';
+import Button from './components/Button/Button';
 
 function App() {
+  const {counter} = useSelector(state => state);
+  const dispatch = useDispatch();  
+  
   return (
     <div className="App">
-      <h1 className='text-2xl text-black'>hello world</h1>
+      <h1 className='text-2xl text-black'>{counter.value}</h1>
+      <Button className='bg-[#e31c58] text-white px-3 py-2 rounded-[5px] mr-2' onclick={() => dispatch(decrement())} text="decrement" />
+      <Button className='bg-[#0ae715] px-3 py-2 rounded-[5px]' text="Increment" onclick={() => dispatch(increment())} />
     </div>
   );
 }
