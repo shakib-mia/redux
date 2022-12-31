af2492e055d2c460d4f2d49d308ffd544efb415e
shakib-mia <smdshakibmia2001@gmail.com>
2022-12-31 21:38:11 +0600

    counter added

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <h1 className='text-2xl text-black'>{counter.value}</h1>
       <Button className='bg-[#e31c58] text-white px-3 py-2 rounded-[5px] mr-2' onclick={() => dispatch(decrement())} text="decrement" />
       <Button className='bg-[#0ae715] px-3 py-2 rounded-[5px]' text="Increment" onclick={() => dispatch(increment())} />
+      
     </div>
   );
 }
